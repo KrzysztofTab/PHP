332e32d27b440bcbe874a29ae383630ea0232efd
Krzysztof <k.tabor@wp.pl>
2020-06-19 22:25:13 +0200

    Create uzytkownicy.sql

---
+++ Rejstracja/uzytkownicy.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3325
+-- Czas generowania: 19 Cze 2020, 22:22
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `osadnicy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `id` int(11) NOT NULL,
+  `user` text COLLATE utf8_polish_ci NOT NULL,
+  `pass` text COLLATE utf8_polish_ci NOT NULL,
+  `email` text COLLATE utf8_polish_ci NOT NULL,
+  `drewno` int(11) NOT NULL,
+  `kamien` int(11) NOT NULL,
+  `zboze` int(11) NOT NULL,
+  `dnipremium` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id`, `user`, `pass`, `email`, `drewno`, `kamien`, `zboze`, `dnipremium`) VALUES
+(1, 'Adam', '$2y$10$7W22zxhqzblZ9MBU8rxbyu91kWGshQfUrbNm06rG2STpo5roPPOCu', 'adam@gmail.com', 213, 5675, 342, 0),
+(2, 'Marek', 'asdfg', 'marek@gmail.com', 324, 1123, 4325, 15),
+(3, 'Anna', 'zxcvb', 'anna@gmail.com', 4536, 17, 120, 25),
+(4, 'Andrzej', 'asdfg', 'andrzej@gmail.com', 5465, 132, 189, 0),
+(5, 'Justyna', 'yuiop', 'justyna@gmail.com', 245, 890, 554, 0),
+(6, 'Kasia', 'hjkkl', 'kasia@gmail.com', 267, 980, 109, 12),
+(7, 'Beata', 'fgthj', 'beata@gmail.com', 565, 356, 447, 77),
+(8, 'Jakub', 'ertyu', 'jakub@gmail.com', 2467, 557, 876, 0),
+(9, 'Janusz', 'cvbnm', 'janusz@gmail.com', 65, 456, 2467, 0),
+(10, 'Roman', 'dfghj', 'roman@gmail.com', 97, 226, 245, 23),
+(11, 'Kriss', '$2y$10$5GJei0laxEVWwMF4UOMZpuxV.pMiAT7/3JTef4mhYPQDsLzuHRi/S', 'k.tabor@outlook.com', 100, 100, 100, 14);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
